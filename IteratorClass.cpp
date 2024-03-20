4e4ad8e4c191ccbb54977c34b27ea2cd4988224b
Anshuman Tiwari <lazycodebaker@outlook.com>
2024-03-20 20:21:39 +0530

    class as iterator

--- IteratorClass.cpp
+++ IteratorClass.cpp
@@ -1,53 +1,31 @@
-#include "iostream"
+#include <iostream>
+#include <memory>
 
 template <class T>
 class Ring
 {
 public:
     class iterator;
-    Ring(size_t size) : m_pos(0), m_size(size), m_values(NULL)
-    {
-        m_values = new T[size];
-    };
+    Ring(size_t size) : m_pos(0), m_size(size), m_values(std::make_unique<T[]>(size)) {}
 
-    ~Ring()
-    {
-        delete[] m_values;
-    };
+    int size() const noexcept { return m_size; }
 
-    int size()
-    {
-        return m_size;
-    };
+    iterator begin() const noexcept { return iterator(0, *this); }
 
-    iterator begin()
-    {
-        return iterator(0, *this);
-    };
+    iterator end() const noexcept { return iterator(m_size, *this); }
 
-    iterator end()
+    void add(T value) noexcept
     {
-        return iterator(m_size, *this);
-    };
-
-    void add(T value)
-    {
-        m_values[m_pos++] = value;
-
+        m_values[m_pos++] = std::move(value);
         if (m_pos == m_size)
-        {
             m_pos = 0;
-        };
-    };
+    }
 
-    T &get(int pos)
-    {
-        return m_values[pos];
-    };
+    const T &get(int pos) const noexcept { return m_values[pos]; }
 
 private:
     int m_pos, m_size;
-    T *m_values;
+    std::unique_ptr<T[]> m_values;
 };
 
 template <class T>
@@ -55,31 +33,33 @@ class Ring<T>::iterator
 {
 private:
     int m_pos;
-    Ring<T> &m_ring;
+    const Ring<T> &m_ring;
 
 public:
-    iterator(int pos, Ring &a_ring) : m_pos(pos), m_ring(a_ring){};
+    iterator(int pos, const Ring<T> &a_ring) noexcept : m_pos(pos), m_ring(a_ring) {}
 
-    iterator &operator++(int)
+    iterator &operator++() noexcept
     {
-        m_pos++;
+        ++m_pos;
         return *this;
-    };
+    }
 
-    T &operator*()
+    iterator operator++(int) noexcept
     {
-        return m_ring.get(m_pos);
-    };
+        iterator temp = *this;
+        ++(*this);
+        return temp;
+    }
 
-    bool operator!=(const iterator &other) const
-    {
-        return m_pos != other.m_pos;
-    };
+    const T &operator*() const noexcept { return m_ring.get(m_pos); }
+
+    bool operator!=(const iterator &other) const noexcept { return m_pos != other.m_pos; }
 };
 
 int main()
 {
-    Ring<int> r(5);
+    constexpr size_t RingSize = 5;
+    Ring<int> r(RingSize);
 
     r.add(1);
     r.add(2);
@@ -87,9 +67,10 @@ int main()
     r.add(4);
     r.add(5);
 
-    for (Ring<int>::iterator it = r.begin(); it != r.end(); it++)
+    // Using range-based for loop for iteration
+    for (const auto &element : r)
     {
-        std::cout << *it << std::endl;
+        std::cout << element << std::endl;
     };
 
     return EXIT_SUCCESS;
